d691cd9cb3fcb64731c800f01ed18966cf261c43
HBIDamian <hbidamian@gmail.com>
2021-08-06 03:03:10 +0100

    Threw some glitter on the shit, and polished it.

--- Non Flashy Clock.bat
+++ Non Flashy Clock.bat
@@ -1,16 +1,33 @@
 @echo off
-:: This was made because if a program is on full screen on my main screen,
-:: I can put this program on my other screen so I can look at the time. 
 cls
+:: Edit this to choose your prefered time mode.
+::     "0" = Time Format HH:mm:ss
+::     "1" = Time Format HH:mm:ss:SS
+:: Default: "0"
+set /A timeChoice=0
+
 :TOP
 :: Clear Text without using CLS (So there are no flash)
 for /F "tokens=1 delims=# " %%a in ('"prompt #$H# & echo on & for %%b in (1) do rem"') do set "BSPACE=%%a"
 :: Provides the means to hide the mouse cursor using %esc%[?25l
 for /F "delims=#" %%b in ('prompt #$E# ^& for %%b in ^(1^) do rem') do set esc=%%b
 
-:: Change time to replace the text below with %time%%esc%[?25l if you want to see miliseconds too.
-:: Default: %time:~-11,2%:%time:~-8,2%:%time:~-5,2%%esc%[?25l
-<nul set /p =%time:~-11,2%:%time:~-8,2%:%time:~-5,2%%esc%[?25l
-title %time:~-11,2%:%time:~-8,2%:%time:~-5,2%
-<nul set /p =%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%
+::Set time variables
+set HH=%time:~-11,2%
+set mm=%time:~-8,2%
+set ss=%time:~-5,2%
+set HHmmss=%HH: =0%:%mm%:%ss%
+
+:: Handles the settings for what time mode a user wants
+set timeMode=%HHmmss%
+if /i "%timeChoice%"=="0" set timeMode=%HHmmss%
+if /i "%timeChoice%"=="1" set timeMode=%time: =0%
+
+:: What you see is what you get.
+<nul set /p =%timeMode%%esc%[?25l
+title %timeMode%
+<nul set /p =%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%%BSPACE%
 goto :TOP
+
+:: This was made because if a program is on full screen on my main screen,
+:: I can put this program on my other screen so I can look at the time. 
